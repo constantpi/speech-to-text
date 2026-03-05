a3cd64541f9312b13b1118aa37f68d5a37db6d51
constantpi <wada.zkai@gmail.com>
2026-03-06 02:10:08 +0900

    文の区切りを自動で見つける

--- speech_to_text/audio_transcriber.py
+++ speech_to_text/audio_transcriber.py
@@ -9,7 +9,7 @@ from faster_whisper import WhisperModel
 from concurrent.futures import ThreadPoolExecutor
 
 from .utils.audio_utils import create_audio_stream
-from .utils.word_merge import word_merge
+from .utils.word_merge import word_merge, clean_word_list
 from .vad import Vad
 from .utils.file_utils import write_audio
 from .websoket_server import WebSocketServer
@@ -17,6 +17,13 @@ from .openai_api import OpenAIAPI
 
 RATE = 16000
 CHUNK = 512
+
+
+class AudioData:
+    def __init__(self, data: np.ndarray, start_time: float, is_last: bool):
+        self.audio_data = data
+        self.start_time = start_time
+        self.is_last = is_last
 
 
 class AppOptions(NamedTuple):
@@ -53,65 +60,19 @@ class AudioTranscriber:
         self.silence_counter: int = 0
         self.audio_data_list = []
         self.all_audio_data_list = []
-        self.audio_queue = queue.Queue()
+        # self.audio_queue = queue.Queue()
         self.transcribing = False
         self.stream = None
         self._running = asyncio.Event()
-        self._transcribe_task = None
         self._recent_transcribe_task = None
         self._translate_task = None
         # 直近の数秒間の音声データ
-        self.recent_audio_data: Optional[tuple[np.ndarray, float]] = None
+        self.recent_audio_data: Optional[AudioData] = None
         self.recent_audio_start: int = 0
         self.recent_audio_length: int = 0
         self.transcribe_result_list: list[str] = []  # 文字起こし結果のリスト(3つ程度保持しておく)
         self.texts_to_translate: list[str] = []  # 翻訳待ちのテキストのリスト
         self.word_timestamp_list: list[list[tuple[float, float, str]]] = []  # start, end, textのタプルのリストのリスト
-
-    async def transcribe_audio(self):
-        # Ignore parameters that affect performance
-        transcribe_settings = self.transcribe_settings.copy()
-        transcribe_settings["without_timestamps"] = True
-        transcribe_settings["word_timestamps"] = False
-
-        with ThreadPoolExecutor() as executor:
-            while self.transcribing:
-                try:
-                    # Get audio data from queue with a timeout
-                    audio_data = await self.event_loop.run_in_executor(
-                        executor, functools.partial(self.audio_queue.get, timeout=3.0)
-                    )
-
-                    # Create a partial function for the model's transcribe method
-                    func = functools.partial(
-                        self.whisper_model.transcribe,
-                        audio=audio_data,
-                        **transcribe_settings,
-                    )
-
-                    # Run the transcribe method in a thread
-                    segments, _ = await self.event_loop.run_in_executor(executor, func)
-
-                    text = "\n".join([segment.text for segment in segments])
-                    eel.display_transcription(text)
-                    if self.websocket_server is not None:
-                        await self.websocket_server.send_message(text)
-
-                    # textをself.transcribe_result_listに追加し、最大でapp_options.save_result_number個まで保持する
-                    self.transcribe_result_list.append(text)
-                    eel.display_recent_transcription("\n".join(self.transcribe_result_list))  # 直近の文字起こし結果を結合して表示 一つ多い状態でOK
-                    if len(self.transcribe_result_list) > self.app_options.save_result_number:
-                        self.transcribe_result_list = self.transcribe_result_list[-self.app_options.save_result_number:]
-
-                    # 翻訳待ちのテキストに追加する
-                    if self.app_options.use_openai_api:
-                        self.texts_to_translate.append(text)
-
-                except queue.Empty:
-                    # Skip to the next iteration if a timeout occurs
-                    continue
-                except Exception as e:
-                    eel.on_recive_message(str(e))
 
     async def transcribe_recent_audio(self):
         """
@@ -134,12 +95,13 @@ class AudioTranscriber:
                     continue
 
                 try:
-                    audio, start_time = self.recent_audio_data
+                    audio, start_time, is_last = self.recent_audio_data.audio_data, self.recent_audio_data.start_time, self.recent_audio_data.is_last
                     func = functools.partial(
                         self.whisper_model.transcribe,
                         audio=audio,
                         **transcribe_settings,
                     )
+                    self.recent_audio_data = None  # Transcribed recent audio data, reset to None
 
                     segments, _info = await self.event_loop.run_in_executor(executor, func)
                     word_list = []
@@ -154,16 +116,36 @@ class AudioTranscriber:
                                 cleaned_word = word.word.strip().strip("-").strip(".")
                                 if cleaned_word:  # 空でない場合のみ追加
                                     word_list.append((word_start_time, word_end_time, word.word))
-                    self.recent_audio_data = None  # Transcribed recent audio data, reset to None
                     if start_time < 0.01:
                         self.word_timestamp_list.clear()  # 音声区間の開始が0秒付近の場合は、前の区間の単語タイムスタンプをクリア
                     self.word_timestamp_list.append(word_list)  # word_timestamp_listに追加
 
+                    merge_result = word_merge(self.word_timestamp_list, start_time)  # word_timestamp_listをマージしてテキストに追加
+                    if merge_result.determined_text:
+                        print(f"Determined text: '{merge_result.determined_text}', determined_end: {merge_result.determined_end:.2f}s")
+                        eel.display_transcription(merge_result.determined_text)
+                        self.transcribe_result_list.append(merge_result.determined_text)  # transcribe_result_listに追加
+                        # 翻訳待ちのテキストに追加する
+                        if self.app_options.use_openai_api:
+                            self.texts_to_translate.append(merge_result.determined_text)  # 翻訳待ちのテキストに追加
+
+                        # word_timestamp_listをマージして確定したテキストの終わりまでクリア
+                        self.word_timestamp_list = clean_word_list(self.word_timestamp_list, merge_result.determined_end)
+                        print("cleaned:", clean_word_list(self.word_timestamp_list, merge_result.determined_end))
+
                     text = ("\n".join(self.transcribe_result_list) + "\n") if self.transcribe_result_list else ""  # 直近の文字起こし結果を結合
-                    text += word_merge(self.word_timestamp_list)  # word_timestamp_listをマージしてテキストに追加
+                    merged = merge_result.undetermined_text  # マージして確定できなかったテキスト
+                    text += merged
                     eel.display_recent_transcription(text)
-                    if self.websocket_server is not None:
-                        await self.websocket_server.send_message(text)
+
+                    if is_last:
+                        eel.display_transcription(merged)
+                        self.transcribe_result_list.append(merged)  # transcribe_result_listに追加
+                        self.transcribe_result_list = self.transcribe_result_list[-self.app_options.save_result_number:]
+                        # 翻訳待ちのテキストに追加する
+                        if self.app_options.use_openai_api:
+                            self.texts_to_translate.append(merged)  # 翻訳待ちのテキストに追加
+                        self.word_timestamp_list.clear()  # 単語タイムスタンプのリストをクリア
 
                 except Exception as e:
                     eel.on_recive_message(str(e))
@@ -184,27 +166,19 @@ class AudioTranscriber:
             self.recent_audio_length = min(self.recent_audio_length + 1, self.app_options.recent_audio_max_length)
             self.recent_audio_start = len(self.audio_data_list) - self.app_options.recent_audio_duration * self.recent_audio_length
             start_time = self.recent_audio_start * CHUNK / RATE
-            self.recent_audio_data = (np.concatenate(self.audio_data_list[self.recent_audio_start:]), start_time)
-            # print(f"Updated recent audio data: shape={self.recent_audio_data.shape}, length={self.recent_audio_length} seconds")
+            self.recent_audio_data = AudioData(np.concatenate(self.audio_data_list[self.recent_audio_start:]), start_time, False)
 
         if not is_speech and self.silence_counter > self.app_options.silence_limit:
+            if len(self.audio_data_list) > self.app_options.noise_threshold:
+                # 音声区間の終了とみなす
+                # 若干長めになる可能性はあるが誤差の範囲のためOKとする
+                start_time = self.recent_audio_start * CHUNK / RATE
+                self.recent_audio_data = AudioData(np.concatenate(self.audio_data_list[self.recent_audio_start:]), start_time, True)
             self.silence_counter = 0
             self.recent_audio_length = 0
             self.recent_audio_start = 0
-            self.recent_audio_data = None
-            self.word_timestamp_list.clear()  # 音声区間が終了したら、word_timestamp_listもクリア
 
-            # if self.app_options.create_audio_file:
-            #     self.all_audio_data_list.extend(self.audio_data_list)
-
-            if len(self.audio_data_list) > self.app_options.noise_threshold:
-                # print(f"Detected speech segment with {len(self.audio_data_list)} audio chunks.")
-                concatenate_audio_data = np.concatenate(self.audio_data_list)
-                self.audio_data_list.clear()
-                self.audio_queue.put(concatenate_audio_data)
-            else:
-                # noise clear
-                self.audio_data_list.clear()
+            self.audio_data_list.clear()
 
     def batch_transcribe_audio(self, audio_data: np.ndarray):
         segment_list = []
@@ -250,9 +224,6 @@ class AudioTranscriber:
             )
             self.stream.start()
             self._running.set()
-            self._transcribe_task = asyncio.run_coroutine_threadsafe(
-                self.transcribe_audio(), self.event_loop
-            )
             # start recent-audio transcribe task (runs similarly to transcribe_audio)
             self._recent_transcribe_task = asyncio.run_coroutine_threadsafe(
                 self.transcribe_recent_audio(), self.event_loop
@@ -270,9 +241,6 @@ class AudioTranscriber:
     async def stop_transcription(self):
         try:
             self.transcribing = False
-            if self._transcribe_task is not None:
-                self.event_loop.call_soon_threadsafe(self._transcribe_task.cancel)
-                self._transcribe_task = None
             if self._recent_transcribe_task is not None:
                 self.event_loop.call_soon_threadsafe(self._recent_transcribe_task.cancel)
                 self._recent_transcribe_task = None
--- speech_to_text/utils/word_merge.py
+++ speech_to_text/utils/word_merge.py
@@ -1,6 +1,13 @@
 THRESHOLD = 0.2  # クラスタリングの閾値（秒）
 
 
+class MergeResult:
+    def __init__(self, determined_text: str, determined_end: float, undetermined_text: str):
+        self.determined_text = determined_text
+        self.determined_end = determined_end
+        self.undetermined_text = undetermined_text
+
+
 def extract_alphabet(word: str) -> str:
     '''単語からアルファベットのみを抽出する'''
     return "".join([c for c in word if c.isalpha()])
@@ -23,16 +30,30 @@ class Class:
         self.middle = (self.middle * (len(self.word_list) - 1) + (start + end) / 2) / len(self.word_list)
 
 
-def word_merge(word_list: list[list[tuple[float, float, str]]]) -> str:
+def word_merge(word_list: list[list[tuple[float, float, str]]], audio_start: float) -> MergeResult:
+
+    # 調整のためにaudio_startを1秒前倒しする。そうしないと本当にピリオドかどうかの判定が難しいため
+    audio_start -= 1.0
+
+    # 事前におかしな検出を削除しておく。
+    word_list = clean_word_list(word_list, 0.0)
+
     flatten_list: list[tuple[float, float, str, int]] = []  # start, end, text, index
     for i, sublist in enumerate(word_list):
         texts = "".join([text for _, _, text in sublist])
+        print(f"sublist {i}: {texts}")
+        # もしsublistのすべてにおいてstart==endであったら、sublistをスキップする
         for start, end, text in sublist:
+            # もしtextが単語+...のようになっている場合は、textからアルファベットのみを抽出する
+            if text.endswith("..."):
+                text = extract_alphabet(text[:-3])
+            elif text.startswith("..."):
+                text = extract_alphabet(text[3:])
             flatten_list.append((start, end, text, i))
     # 区間の中央値でソートする
     flatten_list.sort(key=lambda x: (x[0] + x[1]) / 2)
     if not flatten_list:
-        return ""
+        return MergeResult(determined_text="", determined_end=0.0, undetermined_text="")
     max_end = max(flatten_list, key=lambda x: x[1])[1]
     # クラスタリングする
     clusters: list[Class] = []
@@ -61,26 +82,76 @@ def word_merge(word_list: list[list[tuple[float, float, str]]]) -> str:
             clusters.append(Class(start, end, text, index))
     # clustersをmiddleでソートする
     clusters.sort(key=lambda x: x.middle)
-    result = []
+    result: list[tuple[str, float]] = []
     for cluster in clusters:
+        max_end = max(cluster.range_list, key=lambda x: x[1])[1]
         num = len(cluster.word_list)
         if num == 1:
-            result.append(cluster.word_list[0][0])
+            result.append((cluster.word_list[0][0], max_end))
         elif num == 2:
             # このクラスタが全体から見て前半にあるか後半にあるかで、前半ならindexが小さい方、後半ならindexが大きい方を採用する
             if cluster.middle < max_end / 2:
-                result.append(cluster.word_list[0][0])
+                result.append((cluster.word_list[0][0], max_end))
             else:
-                result.append(cluster.word_list[1][0])
+                result.append((cluster.word_list[1][0], max_end))
         else:
             # clusterの中でindexが小さい順にtextを結合する
             sorted_word_list = sorted(cluster.word_list, key=lambda x: x[1])
             # 中央値を採用する
-            result.append(sorted_word_list[num // 2][0])
+            result.append((sorted_word_list[num // 2][0], max_end))
     # 同じ単語が連続している場合は1つにまとめる
-    final_result = []
-    for word in result:
-        if not final_result or not is_same_word(final_result[-1], word):
-            final_result.append(word)
+    final_result: list[tuple[str, float]] = []
+    for r in result:
+        if not final_result or not is_same_word(final_result[-1][0], r[0]):
+            final_result.append(r)
+        if is_same_word(final_result[-1][0], r[0]):
+            final_result[-1] = (final_result[-1][0], max(final_result[-1][1], r[1]))
+    is_determined = True
+    determined_end = 0.0
+    determined_length = 0
+    for i, (word, end) in enumerate(final_result):
+        if is_determined:
+            is_determined = audio_start > end
+        if is_determined:
+            # 単語の終わりがピリオドであったら、そこで確定とする
+            if word.endswith(".") or word.endswith("?") or word.endswith("!"):
+                determined_end = end
+                determined_length = i + 1
 
-    return "".join(final_result)
+    print("determined_text:", "".join(word for word, _ in final_result[:determined_length]))
+    print("undetermined_text:", "".join(word for word, _ in final_result[determined_length:]))
+
+    return MergeResult(
+        determined_text="".join(word for word, _ in final_result[:determined_length]),
+        determined_end=determined_end,
+        undetermined_text="".join(word for word, _ in final_result[determined_length:])
+    )
+
+
+def clean_word_list(word_list: list[list[tuple[float, float, str]]], determined_end: float) -> list[list[tuple[float, float, str]]]:
+    '''word_listから、determined_endより前の単語を削除する'''
+    new_word_list: list[list[tuple[float, float, str]]] = []
+    for sublist in word_list:
+        if not sublist:
+            continue
+        new_sublist = []
+        start_time_list = [start for start, _, _ in sublist]
+        end_time_list = [end for _, end, _ in sublist]
+        if start_time_list == end_time_list:
+            # すべての単語が0秒で検出されている場合は、誤検出の可能性が高いためスキップする
+            print(f"skipping sublist because all start and end are the same: {sublist}")
+            continue
+        first_start = sublist[0][0]
+        first_end = sublist[0][1]
+        if len(sublist) > 1:
+            if all(start == first_start for start, _, _ in sublist) or all(end == first_end for _, end, _ in sublist):
+                # すべて同じ位置に検出されている単語は、誤検出の可能性が高いためスキップする
+                print(f"skipping sublist because all start or end are the same: {sublist}")
+                continue
+
+        for start, end, text in sublist:
+            if start >= determined_end - 0.01:  # determined_endの少し前から残す
+                new_sublist.append((start, end, text))
+        if new_sublist:
+            new_word_list.append(new_sublist)
+    return new_word_list
